412ed26661a0a10522f5734c7824a16339fa8ca5
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-02-22 13:25:35 -0500

    prac

--- app/serializers/instructor_serializer.rb
+++ app/serializers/instructor_serializer.rb
@@ -1,3 +1,5 @@
 class InstructorSerializer < ActiveModel::Serializer
   attributes :id, :name, :created_at, :updated_at
+
+  has_many :students
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,3 +1,5 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :name, :major, :age, :instructor_id, :created_at, :updated_at
+
+  belongs_to :instructor
 end
